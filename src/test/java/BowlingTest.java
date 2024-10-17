ddd99b0670a80b3b13d017643ca041c9357cba5a
venkatesh balakrishnan <venkatesh.balakrishnan@diconium.com>
2024-10-17 10:30:21 +0200

    Make it ready for Bowling Game

---
+++ src/test/java/BowlingTest.java
@@ -0,0 +1,12 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BowlingTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(0, 0);
+    }
+
+}
