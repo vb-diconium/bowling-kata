32c94de76b1597c3daf79e309ef8b1d4cf24a23a
gomathi-shankar-diconium <gomathi.lakshmanan@diconium.com>
2024-10-17 20:23:09 +0530

    Strick

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -3,6 +3,18 @@ package bowling;
 public class Bowling {
 
     public void roll(int pins) {
+        int rollScore = 0;
+        int rollCount = 0;
+        switch (pins) {
+            case 10:
+                rollScore = 10;
+            case 0:
+                rollScore = 0;
+
+
+        }
+
+
         throw new UnsupportedOperationException("notImplemented() cannot be performed because ...");
     }
 
--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -2,6 +2,7 @@ package bowling;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BowlingTest {
@@ -9,6 +10,16 @@ class BowlingTest {
     @Test
     void test() {
         assertEquals(0, 0);
+    }
+
+    @Test
+    void testBowlingForFirstStrickAndRestAllAreZeroScore(){
+        Bowling bowling = new Bowling();
+        bowling.roll(10);
+        for (int i = 1; i <= 18; i++) {
+            bowling.roll(0);
+        }
+        assertEquals(10,bowling.score());
     }
 
 }
