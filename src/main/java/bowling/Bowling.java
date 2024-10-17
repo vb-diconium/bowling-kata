e2e77f8ed932bcaf5682a34cc13f928bf4e62af1
gomathi-shankar-diconium <gomathi.lakshmanan@diconium.com>
2024-10-17 20:32:14 +0530

    Strick

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -2,24 +2,14 @@ package bowling;
 
 public class Bowling {
 
+    private int totalRollScore = 0;
+
     public void roll(int pins) {
-        int rollScore = 0;
-        int rollCount = 0;
-        switch (pins) {
-            case 10:
-                rollScore = 10;
-            case 0:
-                rollScore = 0;
-
-
-        }
-
-
-        throw new UnsupportedOperationException("notImplemented() cannot be performed because ...");
+        this.totalRollScore = this.totalRollScore+pins;
     }
 
     public int score() {
-        return 0;
+        return this.totalRollScore;
     }
 
 }
