e6ccdf5d562ab0ba389c121b86d07e344e6df4c3
bandepra <99339437+dicobandepras@users.noreply.github.com>
2024-10-18 14:27:49 +0200

    testBowlingForOneSpare

--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -18,13 +18,11 @@ class BowlingTest {
   @Test
   void testScoreForPerfectGame() {
     rollingPins(12, 10);
-
     assertEquals(300, bowling.score());
   }
 
   @Test
   void bowlingRollZeroTest() {
-
     rollingPins(20, 0);
     assertEquals(0, bowling.score());
   }
@@ -34,6 +32,14 @@ class BowlingTest {
     bowling.roll(10);
     rollingPins(18, 0);
     assertEquals(10, bowling.score());
+  }
+
+  @Test
+  void testBowlingForOneSpare(){
+    bowling.roll(5);
+    bowling.roll(5);
+    rollingPins(18, 0);
+    assertEquals(10,bowling.score());
   }
 
   private void rollingPins(int noOfRolls, int pins) {
