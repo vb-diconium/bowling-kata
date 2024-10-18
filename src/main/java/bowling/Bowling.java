5c41e46ff44d36512fe20ee35696ba31a3ef25a4
bandepra <99339437+dicobandepras@users.noreply.github.com>
2024-10-18 14:27:35 +0200

    testBowlingForOneSpare

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -3,8 +3,9 @@ package bowling;
 public class Bowling {
 
     private int totalRollScore = 0;
-     int[] rolls = new int[21];
-     int rollIndex=0;
+    int[] rolls = new int[21];
+    int rollIndex = 0;
+
     public void roll(int pins) {
 
         rolls[rollIndex] = pins;
@@ -13,14 +14,13 @@ public class Bowling {
 
     public int score() {
 
-        for( int roll =0;roll<10;roll++ )
-        {
-           if(rolls[roll]==10)
-           {
-               totalRollScore+= rolls[roll] +rolls[roll+1] +rolls[roll+2];
-           }
+        for (int roll = 0; roll < 10; roll++) {
+            if (rolls[roll] == 10) {
+                totalRollScore += rolls[roll] + rolls[roll + 1] + rolls[roll + 2];
+            } else {
+                totalRollScore += rolls[roll];
+            }
         }
-
         return totalRollScore;
     }
 
