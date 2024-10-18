a34f46858ce2b18535c1f28855fe4b8e6637fa3d
gomathi-shankar-diconium <gomathi.lakshmanan@diconium.com>
2024-10-18 18:36:44 +0530

    Game Done

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -2,47 +2,50 @@ package bowling;
 
 public class Bowling {
 
-    private int totalRollScore = 0;
-    int[] rolls = new int[21];
-    int rollIndex = 0;
+  int[] rolls = new int[21];
+  int rollIndex = 0;
 
-    public void roll(int pins) {
+  public void roll(int pins) {
+    rolls[rollIndex] = pins;
+    rollIndex++;
+  }
 
-        rolls[rollIndex] = pins;
-        rollIndex++;
+  public int score() {
+    var index = 0;
+    var score = 0;
+    for (int roll = 0; roll < 10; roll++) {
+      if (isStrike(index)) {
+        score += 10 + strikeBonus(index);
+        index++;
+      } else if (isSpare(index)) {
+        score += 10 + spareBonus(index);
+        index += 2;
+      } else {
+        score += getFrameScore(index);
+        index += 2;
+      }
     }
 
-    public int score() {
-        int index=0;
-        for (int roll = 0; roll < 10; roll++) {
-            if (isStrike(index)) {
-                totalRollScore += 10 + strikeBonus(index);
-                index++;
-            } else if (isSpare(index)) {
-                totalRollScore += 10 + spareBonus(index);
-                index +=2;
-            } else {
-                totalRollScore += rolls[index] +rolls[index+1];
-                index +=2;
-            }
-        }
+    return score;
+  }
 
-        return totalRollScore;
-    }
+  private int getFrameScore(int index) {
+    return rolls[index] + rolls[index + 1];
+  }
 
-    private int spareBonus(int index) {
-        return rolls[index + 2];
-    }
+  private int spareBonus(int index) {
+    return rolls[index + 2];
+  }
 
-    private boolean isSpare(int roll) {
-        return rolls[roll] + rolls[roll + 1] == 10;
-    }
+  private boolean isSpare(int roll) {
+    return rolls[roll] + rolls[roll + 1] == 10;
+  }
 
-    private int strikeBonus(int roll) {
-        return rolls[roll + 1] + rolls[roll + 2];
-    }
+  private int strikeBonus(int roll) {
+    return rolls[roll + 1] + rolls[roll + 2];
+  }
 
-    private boolean isStrike(int roll) {
-        return rolls[roll] == 10;
-    }
+  private boolean isStrike(int roll) {
+    return rolls[roll] == 10;
+  }
 }
--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -1,7 +1,6 @@
 package bowling;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -35,11 +34,11 @@ class BowlingTest {
   }
 
   @Test
-  void testBowlingForOneSpare(){
+  void testBowlingForOneSpare() {
     bowling.roll(5);
     bowling.roll(5);
     rollingPins(18, 0);
-    assertEquals(10,bowling.score());
+    assertEquals(10, bowling.score());
   }
 
   @Test
@@ -48,10 +47,31 @@ class BowlingTest {
     bowling.roll(5);
     bowling.roll(4);
     rollingPins(17, 0);
-    assertEquals(18,bowling.score());
+    assertEquals(18, bowling.score());
   }
 
-  private void rollingPins(int noOfRolls, int pins) {
+
+  @Test
+  void testForAllPinsTobeOne() {
+    rollingPins(20, 1);
+    assertEquals(20, bowling.score());
+  }
+
+    @Test
+    void testForFirstEightStrikeAndOthersZero() {
+      rollingPins(8,10);
+      rollingPins(2,0);
+      assertEquals(210,bowling.score());
+    }
+
+    @Test
+    void testForAllSpares() {
+      rollingPins(20,5);
+      rollingPins(1,10);
+      assertEquals(155,bowling.score());
+    }
+
+    private void rollingPins(int noOfRolls, int pins) {
     for (int i = 1; i <= noOfRolls; i++) {
       bowling.roll(pins);
     }
