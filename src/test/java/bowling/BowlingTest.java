7d3d1ac118de54c1c7d9b932dca73ed07dfeccc6
manikandankarunanidhi <manikandan.karunanidhi@diconium.com>
2024-10-18 14:18:18 +0200

    Test case for All Strike

--- src/test/java/bowling/BowlingTest.java
+++ src/test/java/bowling/BowlingTest.java
@@ -1,40 +1,44 @@
 package bowling;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BowlingTest {
 
-    @Test
+  Bowling bowling;
 
-    void testScoreForPerfectGame(){
-        Bowling bowling = new Bowling();
-        for(int i=1;i<=12;i++){
-            bowling.roll(10);
-        }
+  @BeforeEach
+  void setUp() {
 
-        assertEquals(300,bowling.score());}
+    bowling = new Bowling();
+  }
 
-    @Test
-    void bowlingRollZeroTest() {
-        Bowling bowling = new Bowling();
-        for (int i = 1; i <= 20; i++) {
-            bowling.roll(0);
-        }
-        assertEquals(0, bowling.score());
+  @Test
+  void testScoreForPerfectGame() {
+    rollingPins(12, 10);
+
+    assertEquals(300, bowling.score());
+  }
+
+  @Test
+  void bowlingRollZeroTest() {
+
+    rollingPins(20, 0);
+    assertEquals(0, bowling.score());
+  }
+
+  @Test
+  void testBowlingForFirstStrickAndRestAllAreZeroScore() {
+    bowling.roll(10);
+    rollingPins(18, 0);
+    assertEquals(10, bowling.score());
+  }
+
+  private void rollingPins(int noOfRolls, int pins) {
+    for (int i = 1; i <= noOfRolls; i++) {
+      bowling.roll(pins);
     }
-
-    @Test
-    void testBowlingForFirstStrickAndRestAllAreZeroScore(){
-        Bowling bowling = new Bowling();
-        bowling.roll(10);
-        for (int i = 1; i <= 18; i++) {
-            bowling.roll(0);
-        }
-        assertEquals(10,bowling.score());
-    }
-
-
+  }
 }
